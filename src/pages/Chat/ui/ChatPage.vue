9deb4b47ee1305794ebd69c243a00ef66e14dea7
kairat <imanculove@gmail.com>
2025-06-16 12:08:47 +0600

    fixed vercel 404 crash

--- src/pages/Chat/ui/ChatPage.vue
+++ src/pages/Chat/ui/ChatPage.vue
@@ -127,7 +127,7 @@ function createNewChat() {
   width: calc(100%);
   height: 100%;
   backdrop-filter: blur(4px);
-  z-index: 1101 ; /* выше формы ввода (1100) и ниже кнопки (1200) */
+  z-index: 1101; /* выше формы ввода (1100) и ниже кнопки (1200) */
 }
 .chat-list {
   list-style: none;
